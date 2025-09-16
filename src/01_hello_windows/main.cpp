41169653fbb40839633b36bcac9caa4895d8db8c
han_sq <hhh@.com>
2025-09-16 15:21:15 +0800

    配置好vscode环境和cmake和库  本次提交库是gcc9.2.0和32位

---
+++ src/01_hello_windows/main.cpp
@@ -0,0 +1,23 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Windows", nullptr, nullptr);
+    if (window == nullptr){
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+
+    return 0;
+}
